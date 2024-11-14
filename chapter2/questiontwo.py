f66b455beba03d85ccab806334952400221d59a3
mfonmfon <mfonm579@gmail.com>
2024-11-15 00:15:21 +0100

    done with the second question

---
+++ chapter2/questiontwo.py
@@ -0,0 +1,10 @@
+def compareNumbers(user_number: int):
+    user_number = int(input("Enter any number"))
+    square_numbers = user_number * 2
+    if square_numbers > 100 and square_numbers == 100 and square_numbers != 100 and square_numbers< 100:
+        print("Number must be 100")
+
+    else:
+        print("Thank you for your feedback")
+
+compareNumbers(10)
