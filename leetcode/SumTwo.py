ee00f4f8952120f57ba3acfb02c937f6d0e85f05
mfonmfon <mfonm579@gmail.com>
2024-11-15 13:01:08 +0100

    leetcode task on two sum

--- leetcode/SumTwo.py
+++ leetcode/SumTwo.py
@@ -1,14 +1,13 @@
-import numbers
-
-
 def sum_two_integers_arrays(nums, target):
     number_of_arrays = {}
     for index, num in enumerate(nums):
         result = target - num
         if result in number_of_arrays:
             return [number_of_arrays[result], index]
-        number_of_arrays[result] = index
-newNumbers = [2, 7, 11, 15]
+        else:
+            number_of_arrays[result] = index
+
+newNumbers = [7,9,3,9]
 newTarget = 9
 print(sum_two_integers_arrays(newNumbers, newTarget))
 
